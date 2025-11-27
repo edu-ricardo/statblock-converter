850438898dafe2bd84092d222953a6b987070eeb
eduardo.florencio <eduardo.florencio@linx.com.br>
2025-11-27 20:53:08 -0300

    Ajuste de configuração

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  optimizeDeps: {
-    include: ['linked-dep'],
-  },
-  base: '/statblock-converter/',
-  build: {
-    commonjsOptions: {
-      include: [/linked-dep/, /node_modules/],
-    },
-  },
-})
+  // Set the base URL for your project when deployed to GitHub Pages.
+  // Replace 'YOUR_REPO_NAME' with the actual name of your GitHub repository.
+  // For example, if your repo is 'my-awesome-app', set base: '/my-awesome-app/'
+  base: '/statblock-converter/', 
+});
